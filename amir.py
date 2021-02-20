bf6e24407bc592ee341a6394bec3f5c1df261ce9
amir-awad <amirtarek04@gmail.com>
2021-02-19 22:16:43 -0800

    second commit

---
+++ amir.py
@@ -0,0 +1,15 @@
+num1 = eval(input())
+num2 = eval(input())
+flag=True
+while num1>0:
+    if num1%10 == num2%10:
+        num1=num1//10
+        flag=True
+    else:
+        num2=num2//10
+        flag=False
+
+if flag:
+    print("sub")
+else:
+    print("not sub")
